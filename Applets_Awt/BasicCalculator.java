b2b2a3306f9cbe322564e60168e58826c71bfaf3
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-09-30 21:19:58 +0530

    Event Handling Programs in Java using Applets

---
+++ Applets_Awt/BasicCalculator.java
@@ -0,0 +1,73 @@
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.Choice;
+import java.awt.Color;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class BasicCalculator extends Applet{
+	Button button;
+	Choice choice;
+	TextField t1,t2,result;
+	@Override
+	public void init() {
+		setSize(500,500);
+		t1=new TextField();
+		t2=new TextField();
+		result=new TextField();
+		result.setEditable(false);
+		choice=new Choice();
+		choice.add("---SELECT---");
+		choice.add("+");
+		choice.add("-");
+		choice.add("*");
+		choice.add("/");
+		setBackground(Color.black);
+		button=new Button("Calculate");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String str=choice.getSelectedItem();
+				double a=Integer.parseInt(t1.getText());
+				double b=Integer.parseInt(t2.getText());
+				switch(str){
+					case "*":{
+						result.setText(a*b+"");
+						break;
+					}
+					case "-":{
+						result.setText(a-b+"");
+						break;
+					}
+					case "+":{
+						result.setText(a+b+"");
+						break;
+					}
+					case "/":{
+						try{
+						result.setText(a/b+"");}
+						catch(Exception e){
+							result.setText("divide by zero exception");
+						}
+						break;
+					}
+					default:{
+						result.setText("invalid choice");
+					}
+				}
+			}
+		});
+		add(t1);
+		add(choice);
+		add(t2);
+		add(button);
+		add(result);
+	}
+	@Override
+	public void destroy(){
+		JOptionPane.showMessageDialog(this,"Thankyou!","Byeee!",JOptionPane.PLAIN_MESSAGE);
+	}
+}

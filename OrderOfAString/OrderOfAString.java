2c7da58160c1c55643f6c5a25b88d0f932a7ba42
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-10-06 23:02:49 +0530

    Update OrderOfAString.java

--- OrderOfAString/OrderOfAString.java
+++ OrderOfAString/OrderOfAString.java
@@ -1,15 +1,8 @@
-import javax.swing.JOptionPane;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author AlphaBAT69
  */
+import javax.swing.JOptionPane;
 public class Question1 extends javax.swing.JFrame {
 
     /**

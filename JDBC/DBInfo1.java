610a6b93bd2fd36d39873a6efb52d33456eda058
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-07 01:32:04 +0530

    DBInfo Class 1

--- JDBC/DBInfo1.java
+++ JDBC/DBInfo1.java
@@ -1,4 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBInfo1 {
-
+	static Connection con;
+	static{
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/demoproject","root","root");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }

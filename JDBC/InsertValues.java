1e812e6f62179548b83ea7b62b47bf9010fa1c22
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-07 00:54:25 +0530

    Inserting values to database from command line

--- JDBC/InsertValues.java
+++ JDBC/InsertValues.java
@@ -1,7 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class InsertValues {
-	public static void main(String[] args) throws ClassNotFoundException{
+	public static void main(String[] args) throws ClassNotFoundException,SQLException{
 		//loading the driver
 		Class.forName("com.mysql.jdbc.Driver");
+		//making a connection
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/demoproject", "root", "tanay1998");
+		//query generation
+		String query="INSERT INTO login(username,password,usertype) VALUES(?,?,?)";
+		//input
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Username:\t");
+		String username=sc.nextLine();
+		System.out.print("Enter Password:\t");
+		String password=sc.nextLine();
+		System.out.print("Enter UserType:\t");
+		String usertype=sc.nextLine();
+		
+		//statement preparation
+		PreparedStatement pStmt=con.prepareStatement(query);
+		pStmt.setString(1, username);
+		pStmt.setString(2, password);
+		pStmt.setString(3, usertype);
+		
+		//execution
+		int rowsAffected=pStmt.executeUpdate();
+		
+		System.out.println(rowsAffected+" rows affected!");
+		
+		pStmt.close();
+		sc.close();
+		con.close();
 	}
 }

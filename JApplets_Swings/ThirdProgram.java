aaac8b3cf748e83640314799905a6d2d477d7c67
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-10 03:20:02 +0530

    third program using other UI components

---
+++ JApplets_Swings/ThirdProgram.java
@@ -0,0 +1,30 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+public class ThirdProgram extends JFrame{
+	JList<String> list;
+	JComboBox cb;
+	JScrollPane pane;
+	public ThirdProgram(String str){
+		super(str);
+		setSize(500,500);
+		setLocationRelativeTo(this);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setLayout(new FlowLayout());
+		
+		String[] val={"Item 1","Item 2","Item 3","Item 4","Item 5","Item 6","Item 7","Item 8","Item 9"};
+		list=new JList<>(val);
+		pane=new JScrollPane(list);
+		add(pane);
+		
+		cb=new JComboBox(val);
+		add(cb);
+	}
+	public static void main(String[] args){
+		new ThirdProgram("third").setVisible(true);
+	}
+}

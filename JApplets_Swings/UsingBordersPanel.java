0c693b88ee010e2e571532b73d21dba9b23bead6
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-27 00:10:42 +0530

    panels modification for borders use

---
+++ JApplets_Swings/UsingBordersPanel.java
@@ -0,0 +1,28 @@
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import javax.swing.border.Border;
+public class UsingBordersPanel extends JPanel{
+	JToolBar toolbar;
+	JButton b1,b2,b3,b4,b5,b6;
+	public UsingBordersPanel(Border b){
+		b1=new JButton(new ImageIcon("dna.jpg"));
+		b2=new JButton(new ImageIcon("dna.jpg"));
+		b3=new JButton(new ImageIcon("dna.jpg"));
+		b4=new JButton(new ImageIcon("dna.jpg"));
+		b5=new JButton(new ImageIcon("dna.jpg"));
+		b6=new JButton(new ImageIcon("dna.jpg"));
+		toolbar=new JToolBar();
+		toolbar.add(b1);
+		toolbar.add(b2);
+		toolbar.add(b3);
+		toolbar.add(b4);
+		toolbar.add(b5);
+		toolbar.add(b6);
+		setBorder(b);
+		add(toolbar);
+		toolbar.setEnabled(false);
+		toolbar.setOrientation(JToolBar.VERTICAL);
+	}
+}

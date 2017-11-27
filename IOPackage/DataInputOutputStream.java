3be6c78def4eb3fd21dc631494e845eb5a6d31c1
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-27 17:49:58 +0530

    using data input stream

--- IOPackage/DataInputOutputStream.java
+++ IOPackage/DataInputOutputStream.java
@@ -16,5 +16,17 @@ public class DataInputOutputStream {
 		dos.close();
 		fos.close();
 		
+		FileInputStream fis=new FileInputStream("data.txt");
+		/*int i;
+		while((i=fis.read())!=-1)
+			System.out.print((char)i);*/
+		System.out.println();
+		DataInputStream dis=new DataInputStream(fis);
+		System.out.println(dis.readInt());
+		System.out.println(dis.readDouble());
+		System.out.println(dis.readByte());
+		System.out.println(dis.readChar());
+		System.out.println(dis.readBoolean());
+		
 	}
 }

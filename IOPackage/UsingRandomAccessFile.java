6340cbf8ba99fbdf89cacf0700b6c8c22bd117b8
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-12-05 17:06:19 +0530

    using random access file class

--- IOPackage/UsingRandomAccessFile.java
+++ IOPackage/UsingRandomAccessFile.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 
 public class UsingRandomAccessFile {
@@ -6,5 +8,14 @@ public class UsingRandomAccessFile {
 		RandomAccessFile obj=new RandomAccessFile("combined.txt", "rw");
 		long length=obj.length();
 		System.out.println("length of file is : "+length);
+		obj.seek(length);
+		System.out.println("wirte your message:");
+		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+		String input=reader.readLine();
+		obj.write(13);
+		obj.write(input.getBytes());
+		obj.close();
+		reader.close();
+		System.out.println("Done!");
 	}
 }

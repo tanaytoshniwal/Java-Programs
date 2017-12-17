a44944f48b9000d9d68fc4f48ebc8e0c63972f90
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-12-17 09:29:13 +0530

    adding class reference in array list

---
+++ UtilPackage/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+	int number;
+	String name;
+	public Student(int number, String name){
+		this.number = number;
+		this.name = name;
+	}
+	void print(){
+		System.out.println(number+":::"+name);
+	}
+}

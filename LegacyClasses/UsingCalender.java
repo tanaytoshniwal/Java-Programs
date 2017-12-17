45fd5de310041206413a55066c9bb1960d74c4e2
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-12-17 18:15:21 +0530

    using calendar class

--- LegacyClasses/UsingCalender.java
+++ LegacyClasses/UsingCalender.java
@@ -3,6 +3,14 @@ import java.util.Calendar;
 public class UsingCalender {
 	public static void main(String[] args){
 		Calendar c=Calendar.getInstance();
-		System.out.println(c);
+		//System.out.println(c);
+		System.out.println(c.get(Calendar.DAY_OF_MONTH));
+		System.out.println(c.get(Calendar.MONTH)+1);
+		System.out.println(c.get(Calendar.YEAR));
+		System.out.println(c.get(Calendar.HOUR));
+		System.out.println(c.get(Calendar.MINUTE));
+		System.out.println(c.get(Calendar.SECOND));
+		System.out.println(c.get(Calendar.DAY_OF_MONTH));
+		System.out.println(c.get(Calendar.DAY_OF_MONTH));
 	}
 }
